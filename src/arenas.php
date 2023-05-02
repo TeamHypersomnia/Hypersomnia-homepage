0196bd0f65f38d213be433c5dfd26924f7bc19f8
Ernest Paśnik <hajtusgn@gmail.com>
2023-05-02 20:15:19 +0200

    Update arenas

--- src/arenas.php
+++ src/arenas.php
@@ -5,17 +5,33 @@ require_once 'src/twig.php';
 $maps = [];
 $arenas = array_diff(scandir($arenas_path), array('..', '.'));
 foreach ($arenas as $key => $value) {
-	$path = "$arenas_path/$value/$value.miniature.png";
-	if (file_exists($path)) {
-		$type = pathinfo($path, PATHINFO_EXTENSION);
-		$base64 = base64_encode(file_get_contents($path));
+
+	$miniature = false;
+	$miniature_path = "$arenas_path/$value/miniature.png";
+	if (file_exists($miniature_path)) {
+		$type = pathinfo($miniature_path, PATHINFO_EXTENSION);
+		$base64 = base64_encode(file_get_contents($miniature_path));
 		$miniature = "data:image/$type;base64,$base64";
-	} else {
-		$miniature = false;
 	}
+
+	$author = 'Unknown';
+	$short_description = '';
+	$json_path = "$arenas_path/$value/$value.json";
+	if (file_exists($json_path)) {
+		$json = json_decode(file_get_contents($json_path), true);
+		if (isset($json['about']['author'])) {
+			$author = $json['about']['author'];
+		}
+		if (isset($json['about']['short_description'])) {
+			$short_description = mb_strimwidth($json['about']['short_description'], 0, 50, '(...)');
+		}
+	}
+
 	array_push($maps, [
 		'name' => $value,
-		'miniature' => $miniature
+		'miniature' => $miniature,
+		'author' => $author,
+		'short_description' => $short_description
 	]);
 }
 
