55e27fae82460fbc74f3e65ffb813e3aacac9823
Ernest Paśnik <hajtusgn@gmail.com>
2023-05-03 12:23:01 +0200

    Update arena pages

--- src/arena.php
+++ src/arena.php
@@ -7,8 +7,71 @@ if (file_exists("$arenas_path/$arena") == false) {
 	die();
 }
 
+function time_elapsed_string($datetime, $full = false) {
+	$now = new DateTime;
+	$ago = new DateTime($datetime);
+	$diff = $now->diff($ago);
+
+	$diff->w = floor($diff->d / 7);
+	$diff->d -= $diff->w * 7;
+
+	$string = array(
+		'y' => 'year',
+		'm' => 'month',
+		'w' => 'week',
+		'd' => 'day',
+		'h' => 'hour',
+		'i' => 'minute',
+		's' => 'second',
+	);
+	foreach ($string as $k => &$v) {
+		if ($diff->$k) {
+			$v = $diff->$k . ' ' . $v . ($diff->$k > 1 ? 's' : '');
+		} else {
+			unset($string[$k]);
+		}
+	}
+
+	if (!$full) $string = array_slice($string, 0, 1);
+	return $string ? implode(', ', $string) . ' ago' : 'just now';
+}
+
+$miniature = false;
+$miniature_path = "$arenas_path/$arena/miniature.png";
+if (file_exists($miniature_path)) {
+	$type = pathinfo($miniature_path, PATHINFO_EXTENSION);
+	$base64 = base64_encode(file_get_contents($miniature_path));
+	$miniature = "data:image/$type;base64,$base64";
+}
+
+$version_timestamp = 0;
+$author = 'Unknown';
+$short_description = '';
+$full_description = '';
+$json_path = "$arenas_path/$arena/$arena.json";
+if (file_exists($json_path)) {
+	$json = json_decode(file_get_contents($json_path), true);
+	if (isset($json['meta']['version_timestamp'])) {
+		$version_timestamp = time_elapsed_string($json['meta']['version_timestamp']);
+	}
+	if (isset($json['about']['author'])) {
+		$author = $json['about']['author'];
+	}
+	if (isset($json['about']['short_description'])) {
+		$short_description = $json['about']['short_description'];
+	}
+	if (isset($json['about']['full_description'])) {
+		$full_description = $json['about']['full_description'];
+	}
+}
+
 echo $twig->render('arena.twig', [
 	'url' => $url,
-	'page' => 'Arenas',
-	'arena' => $arena
+	'page' => $arena,
+	'arena' => $arena,
+	'miniature' => $miniature,
+	'version_timestamp' => $version_timestamp,
+	'author' => $author,
+	'short_description' => $short_description,
+	'full_description' => $full_description
 ]);
