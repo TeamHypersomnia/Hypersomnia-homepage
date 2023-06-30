f5552909c014f52fe0917242fcdad0eb34877157
geneotech <patryk.czachurski@gmail.com>
2023-06-30 15:24:01 +0200

    Support map version query

--- src/arenas.php
+++ src/arenas.php
@@ -3,10 +3,13 @@ require_once 'src/config.php';
 require_once 'src/twig.php';
 
 $maps = [];
+$versions = [];
+
 $arenas = array_diff(scandir($arenas_path), array('..', '.'));
 foreach ($arenas as $key => $value) {
 	$author = 'Unknown';
 	$short_description = '';
+	$version_timestamp = '';
 	$json_path = "$arenas_path/$value/$value.json";
 	if (file_exists($json_path)) {
 		$json = json_decode(file_get_contents($json_path), true);
@@ -16,17 +19,27 @@ foreach ($arenas as $key => $value) {
 		if (isset($json['about']['short_description'])) {
 			$short_description = mb_strimwidth($json['about']['short_description'], 0, 50, '(...)');
 		}
+		if (isset($json['meta']['version_timestamp'])) {
+			$version_timestamp = $json['meta']['version_timestamp'];
+			$versions[$value] = $version_timestamp;
+		}
 	}
 
 	array_push($maps, [
 		'name' => $value,
 		'author' => $author,
-		'short_description' => $short_description
+		'short_description' => $short_description,
+		'version_timestamp' => $version_timestamp
 	]);
 }
 
 if (isset($_GET['format']) && $_GET['format'] == 'json') {
 	print(json_encode($maps));
+	exit();
+}
+
+if (isset($_GET['version']) && isset(versions[$_GET['version'])) {
+	print(versions[$_GET['version']]);
 	exit();
 }
 
