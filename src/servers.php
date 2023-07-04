01fc9903974afec4864725eb8b46ca5525b6b98d
Ernest Paśnik <hajtusgn@gmail.com>
2023-07-04 20:38:00 +0200

    Better error handling

--- src/servers.php
+++ src/servers.php
@@ -3,16 +3,19 @@ require_once 'src/config.php';
 require_once 'src/twig.php';
 require_once 'src/common.php';
 
-$servers = json_decode(file_get_contents("http://hypersomnia.xyz:8420/server_list_json"), true);
-
-foreach ($servers as $key => $value) {
-    $servers[$key]['time_hosted_ago'] = time_elapsed_string(date('Y-m-d H:i:s', $value['time_hosted']));
+$content = @file_get_contents("http://hypersomnia.xyz:8420/server_list_json");
+if ($content == false) {
+	$servers = false;
+} else {
+	$servers = json_decode($content, true);
+	foreach ($servers as $key => $value) {
+		$servers[$key]['time_hosted_ago'] = time_elapsed_string(date('Y-m-d H:i:s', $value['time_hosted']));
+	}
+	usort($servers, fn($a, $b) => $b['num_playing'] <=> $a['num_playing']);
 }
-
-usort($servers, fn($a, $b) => $b['num_playing'] <=> $a['num_playing']);
 
 echo $twig->render('servers.twig', [
 	'url' => $url,
 	'page' => 'Servers',
-    'servers' => $servers
+	'servers' => $servers
 ]);
