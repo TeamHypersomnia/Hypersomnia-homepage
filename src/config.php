7989aea20683f581af0ce1776b514147735bf8a6
geneotech <patryk.czachurski@gmail.com>
2023-07-25 17:42:01 +0200

    Repoint to ~/community_arenas

--- src/config.php
+++ src/config.php
@@ -1,4 +1,4 @@
 <?php
 $url = 'https://hypersomnia.xyz/';
 $cache = '/var/www/html/cache';
-$arenas_path = '/home/ubuntu/hypersomnia/content/arenas';
+$arenas_path = '/home/ubuntu/community_arenas';
