16c1f51fbbedaf07a3960eec564ff681cc1c5c4e
Ernest Paśnik <hajtusgn@gmail.com>
2023-08-07 21:53:50 +0200

    Add json responses

--- src/upload.php
+++ src/upload.php
@@ -1,6 +1,5 @@
 <?php
 require_once 'src/config.php';
-require_once 'src/twig.php';
 
 function ensurePathExists($path) {
 	$path = dirname($path);
@@ -15,7 +14,8 @@ function ensurePathExists($path) {
 }
 
 if (!isset($_POST['apikey']) || !isset($_POST['arena']) || !isset($_POST['filename'])) {
-	die("Missing required parameters");
+	$response['error'] = "Missing required parameter.";
+	die(json_encode($response));
 }
 
 $apikey = $_POST['apikey'];
@@ -25,12 +25,14 @@ $filename = $_POST['filename'];
 $path = "$arenas_path/authorized_mappers.json";
 $authorized_mappers = file_get_contents($path);
 if ($authorized_mappers == false) {
-	die("File $arenas_path/authorized_mappers.json does not exist");
+	$response['error'] = "File $arenas_path/authorized_mappers.json does not exist.";
+	die(json_encode($response));
 }
 
 $authorized_mappers = json_decode($authorized_mappers, true);
 if (!isset($authorized_mappers[$apikey])) {
-	die("You are not authorized to upload maps");
+	$response['error'] = "You are not authorized to upload maps.";
+	die(json_encode($response));
 }
 
 $allow_creating_new = false;
@@ -45,17 +47,16 @@ if (isset($authorized_mappers[$apikey]['maps'])) {
 	$arenas = $authorized_mappers[$apikey]['maps'];
 }
 if ($allow_creating_new == false && in_array($arena, $arenas) == false) {
-	die("You are not authorized to create new maps");
+	$response['error'] = "You are not authorized to create new maps.";
+	die(json_encode($response));
 }
 
+$allowed = ['json', 'png', 'jpg', 'gif', 'ogg', 'wav'];
 $ext = pathinfo($_FILES['upload']['name'], PATHINFO_EXTENSION);
-if (!in_array($ext, ['json', 'png', 'jpg', 'gif', 'ogg', 'wav'])) {
-	die("You are not allowed to upload this file type");
-}
-
-$ext = pathinfo($filename, PATHINFO_EXTENSION);
-if (!in_array($ext, ['json', 'png', 'jpg', 'gif', 'ogg', 'wav'])) {
-	die("You are not allowed to upload this file type");
+$ext2 = pathinfo($filename, PATHINFO_EXTENSION);
+if (!in_array($ext, $allowed) || !in_array($ext2, $allowed)) {
+	$response['error'] = "You are not allowed to upload this file type.";
+	die(json_encode($response));
 }
 
 // To prevent the use of .. or . in file paths and avoid directory traversal attacks
@@ -63,10 +64,13 @@ $filename = str_replace('\\', '/', $filename);
 $pathComponents = explode('/', $filename);
 foreach ($pathComponents as $component) {
 	if ($component === '.' || $component === '..') {
-		die("Parameter `filename` is invalid");
+		$response['error'] = "Parameter `filename` is invalid.";
+		die(json_encode($response));
 	}
 }
 
 $desiredPath = "$arenas_path/$arena/$filename";
 ensurePathExists($desiredPath);
 move_uploaded_file($_FILES['upload']['tmp_name'], $desiredPath);
+$response['success'] = "The file has been uploaded.";
+die(json_encode($response));
