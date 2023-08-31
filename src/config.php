b4795c92406c703a5b9665edecb78e74bb264214
Ernest Paśnik <hajtusgn@gmail.com>
2023-08-31 20:10:03 +0200

    Update workflow

--- src/config.php
+++ src/config.php
@@ -1,12 +1,5 @@
 <?php
-if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
-	// testing
-	$url = 'http://localhost/Hypersomnia-homepage/';
-	$cache = false;
-	$arenas_path = 'C:\hypersomnia\content\arenas';
-} else {
-	// production
-	$url = 'https://hypersomnia.xyz/';
-	$cache = '/var/www/html/cache';
-	$arenas_path = '/home/ubuntu/community_arenas';
-}
+// testing
+$url = 'http://localhost/Hypersomnia-homepage/';
+$cache = false;
+$arenas_path = 'C:\hypersomnia\content\arenas';
