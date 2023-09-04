0e03808b296fe70fe801c1095c9d22afcdd0a4a3
Ernest Paśnik <hajtusgn@gmail.com>
2023-09-04 21:52:59 +0200

    Optimization

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,63 +1,47 @@
-function openTab(evt, cityName) {
-  var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-  document.getElementById(cityName).style.display = "block";
-  evt.currentTarget.className += " active";
-}
-
-function topnav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
+function openTab(evt, name) {
+  const tabcontents = document.querySelectorAll('.tabcontent');
+  tabcontents.forEach(tabcontent => tabcontent.style.display = 'none');
+  const tablinks = document.querySelectorAll('.tablinks');
+  tablinks.forEach(tablink => tablink.classList.remove('active'));
+  document.getElementById(name).style.display = 'block';
+  evt.currentTarget.classList.add('active');
 }
 
 function fast() {
-  var fast = document.getElementsByClassName("fast");
-  var strong = document.getElementsByClassName("strong");
+  const fast = document.getElementsByClassName('fast');
+  const strong = document.getElementsByClassName('strong');
   for (let i = 0; i < fast.length; i++) {
-    fast[i].style.display = "inline";
+    fast[i].style.display = 'inline';
   }
   for (let i = 0; i < strong.length; i++) {
-    strong[i].style.display = "none";
+    strong[i].style.display = 'none';
   }
 }
 
 function strong() {
-  var fast = document.getElementsByClassName("fast");
-  var strong = document.getElementsByClassName("strong");
+  const fast = document.getElementsByClassName('fast');
+  const strong = document.getElementsByClassName('strong');
   for (let i = 0; i < fast.length; i++) {
-    fast[i].style.display = "none";
+    fast[i].style.display = 'none';
   }
   for (let i = 0; i < strong.length; i++) {
-    strong[i].style.display = "inline";
+    strong[i].style.display = 'inline';
   }
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-  var videoList = document.querySelectorAll('.video-t li');
-  if (videoList.length > 0) {
-    videoList.forEach(function(videoItem) {
-      var code = videoItem.getAttribute('data-code');
-      videoItem.style.backgroundImage = "url(https://img.youtube.com/vi/" + code + "/0.jpg)";
-      videoItem.addEventListener('click', function() {
-        var youtubeURL = "//www.youtube.com/watch?v=" + code;
-        window.open(youtubeURL, '_blank');
-      });
+document.addEventListener('DOMContentLoaded', function () {
+  const videoList = document.querySelectorAll('.yt li');
+  videoList.forEach(videoItem => {
+    const code = videoItem.getAttribute('data-code');
+    videoItem.style.backgroundImage = `url(https://img.youtube.com/vi/${code}/0.jpg)`;
+    videoItem.addEventListener('click', () => {
+      const youtubeURL = `//www.youtube.com/watch?v=${code}`;
+      window.open(youtubeURL, '_blank');
     });
-  }
+  });
 });
 
-const copyButtons = document.querySelectorAll('.copy-button');
+const copyButtons = document.querySelectorAll('.copy');
 copyButtons.forEach(button => {
   button.addEventListener('click', () => {
     const closestTocopy = button.closest('.tocopy');
@@ -66,10 +50,10 @@ copyButtons.forEach(button => {
     navigator.clipboard.write([textItem]).then(() => {
       button.innerHTML = '<i class="fas fa-check"></i>';
       setTimeout(() => {
-          button.innerHTML = '<i class="fas fa-copy"></i>';
+        button.innerHTML = '<i class="fas fa-copy"></i>';
       }, 1000);
     }).catch(err => {
-        console.error('Unable to copy: ', err);
+      console.error('Unable to copy: ', err);
     });
   });
 });
