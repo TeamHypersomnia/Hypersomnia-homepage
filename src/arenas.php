4d499f7db70b0500748abaa7081cac8e01564233
Ernest Paśnik <hajtusgn@gmail.com>
2023-09-06 18:11:03 +0200

    Update community arenas

--- src/arenas.php
+++ src/arenas.php
@@ -2,11 +2,9 @@
 require_once 'src/config.php';
 require_once 'src/twig.php';
 
-$maps = [];
-$versions = [];
-
-$arenas = array_diff(scandir($arenas_path), array('..', '.'));
-foreach ($arenas as $key => $value) {
+$arenas = [];
+$folders = array_diff(scandir($arenas_path), array('..', '.'));
+foreach ($folders as $key => $value) {
 	if (is_dir("$arenas_path/$value") == false) {
 		continue;
 	}
@@ -25,30 +23,34 @@ foreach ($arenas as $key => $value) {
 		}
 		if (isset($json['meta']['version_timestamp'])) {
 			$version_timestamp = $json['meta']['version_timestamp'];
-			$versions[$value] = $version_timestamp;
 		}
 	}
 
-	array_push($maps, [
+	$arenas[] = [
 		'name' => $value,
 		'author' => $author,
 		'short_description' => $short_description,
 		'version_timestamp' => $version_timestamp
-	]);
+	];
 }
 
 if (isset($_GET['format']) && $_GET['format'] == 'json') {
-	print(json_encode($maps));
-	exit();
+	header('Access-Control-Allow-Origin: *');
+	header('Content-type: application/json; charset=utf-8');
+	die(json_encode($arenas, JSON_PRETTY_PRINT));
 }
 
-if (isset($_GET['version']) && isset($versions[$_GET['version']])) {
-	print($versions[$_GET['version']]);
-	exit();
+if (isset($_GET['version'])) {
+	$key = array_search($_GET['version'], array_column($arenas, 'name'));
+	if ($key !== false) {
+		header('Access-Control-Allow-Origin: *');
+		header('Content-type: text/plain; charset=utf-8');
+		die($arenas[$key]['version_timestamp']);
+	}
 }
 
 echo $twig->render('arenas.twig', [
 	'url' => $url,
 	'page' => 'Arenas',
-	'maps' => $maps
+	'arenas' => $arenas
 ]);
