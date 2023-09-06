0cdd937c6d2ed48325fa79ef504db5eb0a199817
Ernest Paśnik <hajtusgn@gmail.com>
2023-09-06 19:35:35 +0200

    Update statistics

--- src/statistics.php
+++ src/statistics.php
@@ -6,29 +6,11 @@ $statistics = [];
 $content = @file_get_contents('src/data/statistics.json');
 if ($content !== false) {
 	$statistics = json_decode($content, true);
-}
-$statistics = array_slice($statistics, -5, null, true);
-$days = array_keys($statistics);
-foreach ($statistics as $key => $value) {
-	$unique_visitors[] = @$value['unique_visitors'];
-	$github_stars[] = @$value['github_stars'];
-	$github_forks[] = @$value['github_forks'];
-	$youtube_views[] = @$value['youtube_views'];
-	$youtube_subscribers[] = @$value['youtube_subscribers'];
-	$hypersomnia_arenas[] = @$value['hypersomnia_arenas'];
-	$hypersomnia_servers[] = @$value['hypersomnia_servers'];
+	$statistics = array_slice($statistics, -5, null, true);
 }
 
 echo $twig->render('statistics.twig', [
 	'url' => $url,
 	'page' => 'Statistics',
-	'days' => $days,
-	'statistics' => $statistics,
-	'unique_visitors' => $unique_visitors,
-	'github_stars' => $github_stars,
-	'github_forks' => $github_forks,
-	'youtube_views' => $youtube_views,
-	'youtube_subscribers' => $youtube_subscribers,
-	'hypersomnia_arenas' => $hypersomnia_arenas,
-	'hypersomnia_servers' => $hypersomnia_servers
+	'statistics' => $statistics
 ]);
