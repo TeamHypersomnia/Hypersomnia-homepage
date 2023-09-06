9a7e9e4b1db1ba3cb6dfb93c243669a3c14679f1
Ernest Paśnik <hajtusgn@gmail.com>
2023-09-06 16:12:25 +0200

    Add Chart.js

--- src/statistics.php
+++ src/statistics.php
@@ -9,10 +9,26 @@ if ($content !== false) {
 }
 $statistics = array_slice($statistics, -5, null, true);
 $days = array_keys($statistics);
+foreach ($statistics as $key => $value) {
+	$unique_visitors[] = @$value['unique_visitors'];
+	$github_stars[] = @$value['github_stars'];
+	$github_forks[] = @$value['github_forks'];
+	$youtube_views[] = @$value['youtube_views'];
+	$youtube_subscribers[] = @$value['youtube_subscribers'];
+	$hypersomnia_arenas[] = @$value['hypersomnia_arenas'];
+	$hypersomnia_servers[] = @$value['hypersomnia_servers'];
+}
 
 echo $twig->render('statistics.twig', [
 	'url' => $url,
 	'page' => 'Statistics',
 	'days' => $days,
-	'statistics' => $statistics
+	'statistics' => $statistics,
+	'unique_visitors' => $unique_visitors,
+	'github_stars' => $github_stars,
+	'github_forks' => $github_forks,
+	'youtube_views' => $youtube_views,
+	'youtube_subscribers' => $youtube_subscribers,
+	'hypersomnia_arenas' => $hypersomnia_arenas,
+	'hypersomnia_servers' => $hypersomnia_servers
 ]);
