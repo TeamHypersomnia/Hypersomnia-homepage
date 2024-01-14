2c2f045bee92849779723816ffb72b4fe9d07906
geneotech <patryk.czachurski@gmail.com>
2024-01-14 04:36:23 +0100

    Leaderboards: use better-sqlite3

--- src/leaderboards.js
+++ src/leaderboards.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const sqlite3 = require('sqlite3').verbose();
+const Database = require('better-sqlite3');
 
 const router = express.Router();
 const dbPath = process.env.DB_PATH;
@@ -8,42 +8,35 @@ const dbPath = process.env.DB_PATH;
 router.get('/', (req, res) => {
   try {
     // Open the database connection
-    const db = new sqlite3.Database(dbPath);
+    const db = new Database(dbPath);
 
-    // Fetch all players and their match counts
-    db.all('SELECT account_id, mmr, mu, sigma, matches_won, matches_lost, nickname FROM players', (err, rows) => {
-      if (err) {
-        console.error(err.message);
-        return res.status(500).json({ error: 'Internal Server Error' });
-      }
+    // Prepare and execute the query to fetch all players
+    const stmt = db.prepare('SELECT account_id, mmr, mu, sigma, matches_won, matches_lost, nickname FROM players');
+    const rows = stmt.all();
 
-      // Close the database connection
-      db.close();
+    // Create a JSON response with players and match counts
+    const leaderboards = rows.map((row) => ({
+      account_id: row.account_id,
+      nickname: row.nickname,
+      mmr: row.mmr,
+      mu: row.mu,
+      sigma: row.sigma,
+      matches_won: row.matches_won,
+      matches_lost: row.matches_lost
+    }));
 
-      // Create a JSON response with players and match counts
-      const leaderboards = rows.map((row) => ({
-        account_id: row.account_id,
-        nickname: row.nickname,
-        mmr: row.mmr,
-        mu: row.mu,
-        sigma: row.sigma,
-        matches_won: row.matches_won,
-        matches_lost: row.matches_lost
-      }));
+    leaderboards.sort((a, b) => b.matches_won - a.matches_won);
 
-      leaderboards.sort((a, b) => b.matches_won - a.matches_won);
-
-      if (req.query.format !== undefined && req.query.format == 'json') {
-        return res.status(200).json({ leaderboards });
-      }
-      else {
-        res.render('leaderboards', {
-          page: 'Leaderboards',
-          user: req.user,
-          leaderboards: leaderboards
-        });
-      }
-    });
+    if (req.query.format !== undefined && req.query.format == 'json') {
+      return res.status(200).json({ leaderboards });
+    }
+    else {
+      res.render('leaderboards', {
+        page: 'Leaderboards',
+        user: req.user,
+        leaderboards: leaderboards
+      });
+    }
   } catch (error) {
     console.error(error.message);
     return res.status(500).json({ error: 'Internal Server Error' });
