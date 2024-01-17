de52563cb7be04a5cb0b71011c919cd133c03dda
geneotech <patryk.czachurski@gmail.com>
2024-01-17 03:33:17 +0100

    More legible MMR values

--- src/format_delta.js
+++ src/format_delta.js
@@ -1,7 +1,8 @@
 function formatMMRDelta(mmr_delta, monospace = false) {
   const deltaSymbol = mmr_delta >= 0 ? '↑' : '↓';
   const deltaColor = mmr_delta >= 0 ? 'chartreuse' : '#f85e73';
-  const fontFamily = monospace ? 'font-family: monospace;' : '';
+  // Adjust the font size when monospace is true
+  const fontFamily = monospace ? 'font-family: monospace; font-size: 1.2em;' : '';
   return `<span style="color:${deltaColor}; ${fontFamily}">${deltaSymbol}${Math.abs(mmr_delta).toFixed(2)}</span>`;
 }
 
