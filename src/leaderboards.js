058ac41c0b672ea1afd0d6acb6814514f00c3b93
Ernest Paśnik <erpa@gmx.com>
2024-04-12 20:36:28 +0200

    Update Leaderboards

--- src/leaderboards.js
+++ src/leaderboards.js
@@ -4,15 +4,42 @@ const Database = require('better-sqlite3');
 const router = express.Router();
 const dbPath = process.env.DB_PATH;
 
-// Endpoint for fetching leaderboards
 router.get('/', (req, res) => {
   try {
-    // Open the database connection
     const db = new Database(dbPath);
+    const rows_team = db.prepare('SELECT * FROM mmr_team').all();
+    const rows_ffa = db.prepare('SELECT * FROM mmr_ffa').all();
+    const row_reader = (row) => ({
+      account_id: row.account_id,
+      nickname: row.nickname,
+      mmr: row.mmr,
+      mu: row.mu,
+      sigma: row.sigma,
+      matches_won: row.matches_won,
+      matches_lost: row.matches_lost
+    });
 
-    // Prepare and execute the query to fetch all players
+    const leaderboards_team = rows_team.map(row_reader);
+    leaderboards_team.sort((a, b) => b.mmr - a.mmr);
+
+    const leaderboards_ffa = rows_ffa.map(row_reader);
+    leaderboards_ffa.sort((a, b) => b.mmr - a.mmr);
+
+    if (req.query.format !== undefined && req.query.format == 'json') {
+      return res.status(200).json({ leaderboards_team, leaderboards_ffa });
+    } else {
+      return res.redirect('/leaderboards/bomb-defusal');
+    }
+  } catch (error) {
+    console.error(error.message);
+    return res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+router.get('/bomb-defusal', (req, res) => {
+  try {
+    const db = new Database(dbPath);
     const rows_team  = db.prepare('SELECT account_id, mmr, mu, sigma, matches_won, matches_lost, nickname FROM mmr_team').all();
-    const rows_ffa   = db.prepare('SELECT account_id, mmr, mu, sigma, matches_won, matches_lost, nickname FROM mmr_ffa').all();
 
     const row_reader = (row) => ({
       account_id: row.account_id,
@@ -25,20 +52,50 @@ router.get('/', (req, res) => {
     });
 
     const leaderboards_team = rows_team.map(row_reader);
-    const leaderboards_ffa = rows_ffa.map(row_reader);
-
     leaderboards_team.sort((a, b) => b.mmr - a.mmr);
+
+    if (req.query.format !== undefined && req.query.format == 'json') {
+      return res.status(200).json({ leaderboards_team, leaderboards_ffa });
+    } else {
+      res.render('leaderboards', {
+        page: 'Leaderboards',
+        user: req.user,
+        leaderboard_name: 'Bomb Defusal',
+        leaderboards: leaderboards_team
+      });
+    }
+  } catch (error) {
+    console.error(error.message);
+    return res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+router.get('/ffa', (req, res) => {
+  try {
+    const db = new Database(dbPath);
+    const rows_ffa = db.prepare('SELECT account_id, mmr, mu, sigma, matches_won, matches_lost, nickname FROM mmr_ffa').all();
+
+    const row_reader = (row) => ({
+      account_id: row.account_id,
+      nickname: row.nickname,
+      mmr: row.mmr,
+      mu: row.mu,
+      sigma: row.sigma,
+      matches_won: row.matches_won,
+      matches_lost: row.matches_lost
+    });
+
+    const leaderboards_ffa = rows_ffa.map(row_reader);
     leaderboards_ffa.sort( (a, b) => b.mmr - a.mmr);
 
     if (req.query.format !== undefined && req.query.format == 'json') {
       return res.status(200).json({ leaderboards_team, leaderboards_ffa });
-    }
-    else {
+    } else {
       res.render('leaderboards', {
         page: 'Leaderboards',
         user: req.user,
-        leaderboards_team: leaderboards_team,
-        leaderboards_ffa: leaderboards_ffa
+        leaderboard_name: 'FFA',
+        leaderboards: leaderboards_ffa
       });
     }
   } catch (error) {
