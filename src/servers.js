69f85caccb11b0188dadcf6eaf652fcca4605bfc
Ernest Paśnik <erpa@gmx.com>
2024-04-12 20:19:58 +0200

    Sort server list by players then server name

--- src/servers.js
+++ src/servers.js
@@ -5,7 +5,7 @@ const moment = require('moment');
 let servers = [];
 
 function fetchServers() {
-  axios.get('http://hypersomnia.xyz:8420/server_list_json')
+  axios.get(process.env.SERVER_LIST_JSON)
     .then(response => {
       servers = response.data;
     })
@@ -17,17 +17,17 @@ fetchServers();
 setInterval(fetchServers, 60000); // 1 min
 
 router.get('/', function (req, res) {
-  const data = servers.map(v => {
-    return {
-      ...v,
-      time_hosted_ago: moment(v.time_hosted * 1000).fromNow(),
-      time_last_heartbeat_ago: moment(v.time_last_heartbeat * 1000).fromNow()
-    };
+  servers.sort((a, b) => {
+    if (a.num_playing !== b.num_playing) {
+      return b.num_playing - a.num_playing;
+    } else {
+      return a.name.localeCompare(b.name);
+    }
   });
   res.render('servers', {
     page: 'Servers',
     user: req.user,
-    servers: data
+    servers: servers
   });
 });
 
